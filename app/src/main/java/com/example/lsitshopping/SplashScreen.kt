5eceece90da585aba753e35373b518f5deeac000
Djibril <djidiop89@gmail.com>
2023-02-08 22:14:55 +0100

    add splash screen

---
+++ app/src/main/java/com/example/lsitshopping/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.example.lsitshopping
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.lsitshopping.navigation.Screen
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.shopping))
+        val logoAnimationState = animateLottieCompositionAsState(composition = composition)
+        LottieAnimation(
+            composition = composition,
+            modifier = Modifier
+                .align(Alignment.Center),
+            contentScale = ContentScale.Crop,
+            progress = { logoAnimationState.progress }
+        )
+        if (logoAnimationState.isAtEnd && logoAnimationState.isPlaying) {
+            navController.navigate(Screen.Home.route)
+        }
+    }
+}
